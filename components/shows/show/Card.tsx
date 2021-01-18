fc09adbe3ce3a4c0563b5c0f4aa193ad28aff028
bpearce@angelo.edu <bpearce@angelo.edu>
2021-01-17 18:46:30 -0600

    updates card style

--- components/shows/show/Card.tsx
+++ components/shows/show/Card.tsx
@@ -1,5 +1,110 @@
-const Card = ({ show }: any) => {
-  return <>Show!!</>;
+import tw, { styled } from "twin.macro";
+import type { Show } from "../../../db/entity";
+
+const Colors = {
+  lightText: "#818c99",
+  text: "#9fadbd",
+};
+const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  background-color: #1f232d;
+  height: 265px;
+  font-family: Overpass, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  ${tw`rounded-sm`};
+`;
+const Poster = styled.img`
+  height: 265px;
+  min-width: 185px;
+  width: 185px;
+  ${tw`rounded-l-sm`}
+`;
+const InfoWrapper = styled.div`
+  ${tw`flex flex-col justify-between min-h-0`}
+`;
+const AirDetails = styled.div`
+  padding: 17px 17px 10px;
+  padding-right: 0px;
+  color: ${Colors.text};
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    visibility: hidden;
+  }
+  :hover::-webkit-scrollbar {
+    position: absolute;
+    visibility: visible;
+    width: 0.3rem;
+  }
+  :hover::-webkit-scrollbar-thumb {
+    background: ${Colors.text};
+    border-radius: 0.5rem;
+  }
+`;
+const EpisodeAiring = styled.div`
+  ${tw`text-xs`}
+  color: ${Colors.lightText};
+`;
+const AirDate = styled.div`
+  ${tw`mt-1 text-lg font-semibold`}
+`;
+const SequelText = styled.div`
+  ${tw`mt-1 text-xs`}
+`;
+const Summary = styled.p`
+  ${tw`mt-1 text-xs`};
+  line-height: 17.6px;
+  color: ${Colors.lightText};
+`;
+const Footer = styled.div`
+  ${tw`flex px-3 place-items-center justify-evenly`};
+  background-color: #191d26;
+  min-height: 44px;
+`;
+
+const Tag = styled.button`
+  ${tw`text-xs font-bold`};
+  background-color: #ebb62d;
+  border-radius: 0.3rem;
+  color: #8a2c0f;
+  height: 20px;
+`;
+const TagText = styled.span`
+  ${tw`m-2`}
+`;
+
+interface CardProps {
+  show: Show;
+}
+const Card = ({ show }: CardProps) => {
+  return (
+    <CardWrapper>
+      <Poster
+        src={`http://image.tmdb.org/t/p/w185${show.posterPath}`}
+        alt={show.name}
+      />
+      <InfoWrapper>
+        <AirDetails>
+          <EpisodeAiring>
+            Ep {show.numSeasons} of {show.numEpisodes} airing in
+          </EpisodeAiring>
+          <AirDate>1 day, 11 hours</AirDate>
+          <SequelText>Sequel to Dr.STONE</SequelText>
+          <Summary>{show.overview}</Summary>
+        </AirDetails>
+        <Footer>
+          {Object.keys(show.externalIds)
+            .slice(0, 3)
+            .map((id, index) => (
+              <Tag key={index}>
+                <TagText>{id}</TagText>
+              </Tag>
+            ))}
+        </Footer>
+      </InfoWrapper>
+    </CardWrapper>
+  );
 };
 
 export default Card;
--- components/shows/shows.tsx
+++ components/shows/shows.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import axiosInstance from "../../lib/axios";
 import { useQuery } from "react-query";
-import { Show } from "../../db/entity";
+import type { Show } from "../../db/entity";
 import Card from "./show/Card";
+import tw, { styled } from "twin.macro";
 
+const ShowWrapper = styled.main`
+  ${tw`grid gap-10 p-4`}
+  grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
+  margin: 0 5vw;
+`;
 const Shows = () => {
   const { isLoading, data } = useQuery("shows", async () => {
     const { data } = await axiosInstance.get("api/hello");
+    data.shows = data.shows.slice(0, 10);
     return data.shows as Show[];
   });
 
   return (
-    <>
+    <ShowWrapper>
       {isLoading
         ? "Loading..."
         : data.map((show, index) => <Card show={show} key={index} />)}
-    </>
+    </ShowWrapper>
   );
 };
 
